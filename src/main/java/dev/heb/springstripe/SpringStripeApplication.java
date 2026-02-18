f23c7a3da953027d16afed2ff41ff583d759e550
hebcodee <herberth.ferreira6@gmail.com>
2026-02-18 14:40:22 -0300

    Initial commit

---
+++ src/main/java/dev/heb/springstripe/SpringStripeApplication.java
@@ -0,0 +1,13 @@
+package dev.heb.springstripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStripeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStripeApplication.class, args);
+    }
+
+}
